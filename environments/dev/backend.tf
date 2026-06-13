d1d14a53e72de6b5bc94eb20695c8c7798eda688
Adam zahi <zahi.adem10@gmail.com>
2026-06-13 23:57:51 +0100

    Initiate project

---
+++ environments/dev/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-iac-project-state"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-west-2"
+
+    dynamodb_table = "terraform-locks"
+
+    encrypt        = true
+  }
+}
