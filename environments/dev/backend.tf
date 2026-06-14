155172911459f40ed4cb9a8c49d53797b545783d
Adam zahi <zahi.adem10@gmail.com>
2026-06-14 12:26:54 +0100

    refactor: update Terraform configuration to use variables for dev environment

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,11 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-iac-project-state"
-    key    = "dev/terraform.tfstate"
-    region = "eu-west-2"
-
+    bucket         = "terraform-iac-project-state"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
-
-    encrypt = true
+    encrypt        = true
   }
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,38 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "project_name" {
+  type    = string
+  default = "smartovate"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  type    = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "ec2_instance_type" {
+  type    = string
+}
+
 variable "db_username" {
   type      = string
   sensitive = true
@@ -6,4 +41,8 @@ variable "db_username" {
 variable "db_password" {
   type      = string
   sensitive = true
+}
+
+variable "db_instance_class" {
+  type    = string
 }
