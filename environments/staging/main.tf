d36cf6201586df4a399bacd5849fb0a6f6cc6040
Adam zahi <zahi.adem10@gmail.com>
2026-07-03 11:57:21 +0100

    refactor: comment out IAM module and instance profile in staging environment

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,8 +1,8 @@
-module "iam" {
-  source       = "../../modules/iam"
-  project_name = var.project_name
-  environment  = var.environment
-}
+# module "iam" {
+#   source       = "../../modules/iam"
+#   project_name = var.project_name
+#   environment  = var.environment
+# }
 
 module "vpc" {
   source               = "../../modules/vpc"
@@ -22,7 +22,7 @@ module "ec2" {
   vpc_id        = module.vpc.vpc_id
   subnet_id     = module.vpc.private_subnet_ids[0]
   instance_type = var.ec2_instance_type
-  iam_instance_profile = module.iam.instance_profile_name
+  # iam_instance_profile = module.iam.instance_profile_name
 }
 
 module "rds" {
